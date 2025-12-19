717ee9e82f359d9edf85d9aaa0f04425d6cded0f
Hudson111-s <145516182+Hudson111-s@users.noreply.github.com>
2025-12-19 00:41:24 -0500

    Add cmdline completion; simplify completions.lua and none_ls.lua; update lazy-lock.json

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -6,14 +6,13 @@ return {
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-path",
         },
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
             require("luasnip.loaders.from_vscode").lazy_load()
-            if vim.snippet then
-                vim.snippet.expand = nil
-            end
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -25,28 +24,11 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-c>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
                 }),
                 sources = cmp.config.sources({
                     { name = "luasnip" },
@@ -54,6 +36,13 @@ return {
                 }, {
                     { name = "buffer" },
                 }),
+            })
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "path" },
+                    { name = "cmdline" },
+                },
             })
         end,
     },
--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -5,29 +5,46 @@ return {
         local null_ls = require("null-ls")
         local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 
+        local function format_with_null_ls(bufnr)
+            vim.lsp.buf.format({
+                bufnr = bufnr or vim.api.nvim_get_current_buf(),
+                async = false,
+                filter = function(client)
+                    return client.name == "null-ls"
+                end,
+            })
+        end
+
         null_ls.setup({
             sources = {
                 require("none-ls.diagnostics.ruff"),
-
-                null_ls.builtins.formatting.stylua,
-
+                null_ls.builtins.formatting.stylua.with({
+                    extra_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                }),
                 null_ls.builtins.formatting.black,
-
-                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "--style", "{IndentWidth:4,UseTab:Never}" },
+                }),
             },
+
             on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({ bufnr = bufnr, async = false })
-                        end,
-                    })
+                if client.name ~= "null-ls" then
+                    return
                 end
+
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        format_with_null_ls(bufnr)
+                    end,
+                })
             end,
         })
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+
+        vim.keymap.set("n", "<leader>f", function()
+            format_with_null_ls()
+        end)
     end,
 }
